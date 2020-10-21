10410d104f6d741c55c92d97e8ee5a074e2b6865
unknown <gutsinbox@gmail.com>
2020-10-21 19:42:31 +0400

    1

---
+++ MAIN.java
@@ -0,0 +1,11 @@
+
+public class MAIN {
+    public static void main(String[] args) {
+        int a = NewMath.fact(5);
+        System.out.println(a);
+        int b = NewMath.abs(-5);
+        System.out.println(b);
+        int c = NewMath.pow(2,2);
+        System.out.println(c);
+    }
+}
